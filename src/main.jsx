eac5073670aa35cbd357c7e3107afc0f6384b277
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-17 23:30:35 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { PlaylistProvider } from "./components/PlaylistContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/MyTube-Frontend/">
+      <PlaylistProvider>
+        <App />
+      </PlaylistProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
