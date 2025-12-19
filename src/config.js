acb77e05b0b28c55b6ea4499c3b348d5229e373a
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-18 21:51:51 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
 
-export const API_BASE = "https://pipedapi.kavin.rocks"; // Official, reliable instance
+export const API_BASE = "https://pipedapi.leptons.xyz"; // Austria, CDN, very reliable in 2025
