9fdf1fa9d5e9a726530e0a04df14516a14e14956
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-19 22:15:41 -0500

    Update PlayerContext.jsx

--- src/contexts/PlayerContext.jsx
+++ src/contexts/PlayerContext.jsx
@@ -1,47 +1,45 @@
 // File: src/contexts/PlayerContext.jsx
-import { createContext, useContext, useRef, useState } from "react";
+import { createContext, useContext, useState, useRef } from "react";
 import Player from "../components/Player";
 
 const PlayerContext = createContext();
 export const usePlayer = () => useContext(PlayerContext);
 
 export function PlayerProvider({ children }) {
-  const playerRef = useRef(null);
   const [currentVideo, setCurrentVideo] = useState(null);
   const [playing, setPlaying] = useState(false);
+  const playerRef = useRef(null);
 
-  const playVideo = (video) => {
+  function playVideo(video) {
     setCurrentVideo(video);
     setPlaying(true);
-  };
+  }
 
-  const pauseVideo = () => setPlaying(false);
-  const stopVideo = () => {
+  function pauseVideo() {
+    setPlaying(false);
+  }
+
+  function stopVideo() {
     setPlaying(false);
     setCurrentVideo(null);
-  };
+  }
 
   return (
     <PlayerContext.Provider
-      value={{
-        playerRef,
-        currentVideo,
-        playing,
-        playVideo,
-        pauseVideo,
-        stopVideo
-      }}
+      value={{ currentVideo, playing, playVideo, pauseVideo, stopVideo }}
     >
       {children}
 
-      {/* Global headless player */}
+      {/* Global hidden audio player */}
       {currentVideo && (
-        <Player
-          ref={playerRef}
-          src={`https://www.youtube.com/watch?v=${currentVideo.id}`}
-          playing={playing}
-          onEnded={stopVideo}
-        />
+        <div style={{ display: "none" }}>
+          <Player
+            ref={playerRef}
+            src={`https://www.youtube.com/watch?v=${currentVideo.id}`}
+            playing={playing}
+            onEnded={stopVideo}
+          />
+        </div>
       )}
     </PlayerContext.Provider>
   );
