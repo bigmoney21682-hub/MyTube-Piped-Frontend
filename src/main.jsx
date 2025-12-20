014de231b314650de5aa258d17f4a34f2bf08a47
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-19 22:28:57 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { PlaylistProvider } from "./contexts/PlaylistContext";
 import { PlayerProvider } from "./contexts/PlayerContext";
+import { PlaylistProvider } from "./contexts/PlaylistContext";
+import { API_KEY } from "./config";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <PlaylistProvider>
-        <PlayerProvider>
-          <App />
-        </PlayerProvider>
-      </PlaylistProvider>
-    </BrowserRouter>
+    <PlaylistProvider>
+      <PlayerProvider>
+        <BrowserRouter>
+          <App apiKey={API_KEY} />
+        </BrowserRouter>
+      </PlayerProvider>
+    </PlaylistProvider>
   </React.StrictMode>
 );
