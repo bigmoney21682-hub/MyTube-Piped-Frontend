3d95f28329460ef0ba5d8e0bfc6a6f80046fb616
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-19 21:25:46 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 // File: src/main.jsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { PlayerProvider } from "./contexts/PlayerContext";
+import { PlayerProvider } from "./components/PlayerContext";
+
+// ✅ Replace with your YouTube Data API v3 key
+const YT_API_KEY = "YOUR_API_KEY_HERE";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <PlayerProvider>
-        <App />
+        <App apiKey={YT_API_KEY} />
       </PlayerProvider>
     </BrowserRouter>
   </React.StrictMode>
