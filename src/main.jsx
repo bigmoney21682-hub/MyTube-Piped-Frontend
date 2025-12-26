34b878855dc4ead63fa692cc5af420c80a881faa
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-26 12:35:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
+/**
+ * File: main.jsx
+ * Path: src/main.jsx
+ * Description: Application entry point. Mounts App into the DOM.
+ */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App.jsx";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
