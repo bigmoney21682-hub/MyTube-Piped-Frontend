5d76350df179de6afd3c5d2f5f238152122ea345
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-26 14:25:51 -0500

    Update routes.jsx

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,20 +1,34 @@
 /**
  * File: routes.jsx
  * Path: src/app/routes.jsx
- * Description: Central routing table for the application.
+ * Description: Central routing table with router event logging.
  */
 
-import { Routes as Switch, Route } from "react-router-dom";
+import { Routes as Switch, Route, useLocation } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Watch from "../pages/Watch/Watch";
+import { useEffect } from "react";
+import { logRouter } from "../debug/debugBus";
 
-bootDebug.info("routes.jsx loaded");
+function RouterEvents() {
+  const location = useLocation();
+
+  useEffect(() => {
+    logRouter("Route changed", { path: location.pathname });
+    bootDebug.info("ROUTER → " + location.pathname);
+  }, [location]);
+
+  return null;
+}
 
 export default function Routes() {
   return (
-    <Switch>
-      <Route path="/" element={<Home />} />
-      <Route path="/watch/:id" element={<Watch />} />
-    </Switch>
+    <>
+      <RouterEvents />
+      <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Switch>
+    </>
   );
 }
