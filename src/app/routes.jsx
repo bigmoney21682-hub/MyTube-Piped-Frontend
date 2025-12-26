ea6a25e458b0053c801ab0d6e6218d71984fa2dd
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-26 12:30:14 -0500

    Update routes.jsx

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,3 +1,18 @@
+/**
+ * File: routes.jsx
+ * Path: src/app/routes.jsx
+ * Description: Central routing table for the application.
+ */
+
+import { Routes as Switch, Route } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Watch from "../pages/Watch/Watch";
+
 export default function Routes() {
-  return null;
+  return (
+    <Switch>
+      <Route path="/" element={<Home />} />
+      <Route path="/watch/:id" element={<Watch />} />
+    </Switch>
+  );
 }
