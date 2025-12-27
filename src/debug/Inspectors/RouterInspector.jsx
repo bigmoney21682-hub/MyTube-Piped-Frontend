8f38536ba57412de5553064e6c6ac15b36c751cb
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-27 12:32:41 -0500

    Create RouterInspector.jsx

---
+++ src/debug/Inspectors/RouterInspector.jsx
@@ -0,0 +1,16 @@
+/**
+ * File: RouterInspector.jsx
+ * Path: src/debug/inspectors/RouterInspector.jsx
+ * Description: Minimal router inspector placeholder.
+ */
+
+import React from "react";
+
+export default function RouterInspector() {
+  return (
+    <div style={{ padding: 12 }}>
+      <h3>Router</h3>
+      <p>Router events will appear here.</p>
+    </div>
+  );
+}
