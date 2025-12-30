bd101aa0ee7a53c90a1c19914dd70422dc9916b6
bigmoney21682-hub <bigmoney21682@gmail.com>
2025-12-30 08:32:32 -0500

    Create Menu.jsx

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,16 @@
+/**
+ * File: Menu.jsx
+ * Path: src/pages/Menu.jsx
+ * Description: Placeholder Menu page.
+ */
+
+import React from "react";
+
+export default function Menu() {
+  return (
+    <div style={{ padding: 16, color: "#fff" }}>
+      <h2>Menu</h2>
+      <p>Menu content coming soon.</p>
+    </div>
+  );
+}
