901d70488592258588b95e48c0c60125fef32e37
bigmoney21682-hub <bigmoney21682@gmail.com>
2026-01-05 00:20:10 -0500

    Update NowPlaying.jsx

--- src/pages/Home/NowPlaying.jsx
+++ src/pages/Home/NowPlaying.jsx
@@ -30,7 +30,7 @@ import { AutonextEngine } from "../../player/AutonextEngine.js";
 import { fetchVideo, fetchRelated, fetchTrending } from "../../api/YouTubeAPI.js";
 import normalizeId from "../../utils/normalizeId.js";
 
-import { GlobalPlayer } from "../../player/GlobalPlayer_v2.js";
+import { GlobalPlayer } from "../../player/GlobalPlayerFix.js";
 
 /* ------------------------------------------------------------
    Shared pill-style button (from Watch.jsx)
